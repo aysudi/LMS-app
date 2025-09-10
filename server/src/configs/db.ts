d0d8dc6b43fc74126d15ab47638e92691107824b
Aysu <aysu@Noutbuk-Aysu.local>
2025-09-10 15:29:41 +0400

    feat: mongodb connected

---
+++ server/src/configs/db.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import config from "./config";
+
+const connectToDB = (app: any) => {
+  if (config.DB_URL) {
+    mongoose
+      .connect(config.DB_URL)
+      .then(() => {
+        console.log("🚀 mongodb connected successfully!");
+        app.listen(config.PORT, () => {
+          console.log(`server running on port: ${config.PORT}`);
+        });
+      })
+      .catch((err) => {
+        console.warn("❌ db connection failed: ", err.message);
+      });
+  }
+};
+
+export default connectToDB;
